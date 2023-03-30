2779f4ba5f75b8469fc0fc2f74be5565fb2886e5
Carolissis <caroolassissss@gmail.com>
2023-03-30 17:37:44 -0300

    testes

---
+++ exercicios curso2.py/ex038.py
@@ -0,0 +1,10 @@
+x = int(input('Digite o priemiro número: '))
+y = int(input('Digite o segundo número: '))
+
+if x > y:
+    print('O primeiro número é maior!')
+elif y > x: 
+    print('O segundo número é maior!')
+else:
+    print('Os números sao iguais!')
+    
