f7d02cb4834356eda393bed9e7c79a53751ea3d8
Carolissis <caroolassissss@gmail.com>
2023-03-30 08:18:17 -0300

    Main

---
+++ exercicios curso.py/aula11.py
@@ -0,0 +1,7 @@
+# teste cores 
+
+print('\033[0;30;41m''teste''\033[0m')
+print('\033[4;33;44m''teste''\033[0m')
+print('\033[1;35;43m''teste''\033[0m')
+print('\033[7;30;42m''teste''\033[0m')
+print('(\033[4;35;46m''teste''\033[0m')
---
+++ exercicios curso2.py/ex036.py
@@ -0,0 +1,9 @@
+#aprovando emprestimo 
+c= int(input('Qual o valor da casa?' ))
+s= int(input('Qual o salário do comprador? '))
+a= int(input('Em quantos anos o financiamento? '))
+print('Para pagar uma casa de R${:.2f} em {} anos a prestacao devará ser de {}.')
+if :
+    print('\033[32m''Emprestimo aceito!''\033[m')
+else:
+    print('\033[31m''Emprestimo negado!''\033[m')
