fd2682c124bcc240842393ac244b275d65704177
Carolissis <caroolassissss@gmail.com>
2023-04-03 19:38:27 -0300

    teste 49/50

--- exercicios curso2.py/ex049.py
+++ exercicios curso2.py/ex049.py
@@ -1,4 +1,4 @@
-#tabuada 
+#tabuada  
 num = int(input('Digite um número para ver sua tabuada: '))
 for n in range(1,11):
     print('{} X {:2} = {}'.format(num, n, num*n))
---
+++ exercicios curso2.py/ex050.py
@@ -0,0 +1,9 @@
+#Soma dos pares 
+s = 0
+count = 0
+for c in range(1, 7):
+    num = int(input('Digite o {} valor: '.format(c)))
+    if num %2 == 0:
+        s += num
+        count+= 1
+print('Você informou {} números pares e a soma foi {}'.format(count, s))
