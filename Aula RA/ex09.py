8fcec5f007e68e80e143d7c2d9776bc9830bded2
Carolissis <caroolassissss@gmail.com>
2023-04-04 14:42:59 -0300

    testes

--- Aula RA/ex09.py
+++ Aula RA/ex09.py
@@ -14,8 +14,27 @@ soma = 0
 for c in range(9):
     soma+= CPF[c]*(10-c)
 
-pdv= (soma*10)/11
-if CPF[10] == pdv:
-    print('Correto')
-else:
+x = (soma*10)
+resto = x%11 
+
+if resto == CPF[9]:
+    print('Verificado')
+elif resto == 10:
+    if CPF[9]== 0:
+        print('Verificado')
+else: 
     print('Nao verificado')
+print('O resultado da divisão acima é {} e o resto é {}'.format(soma, resto))
+
+soma2= 0
+for i in range(9):
+    soma2+= CPF[i]*(11-i)
+
+y= (soma2*10)
+resto2= y%11
+
+if resto2 == CPF[10]:
+    print('Verificado')
+else:
+    print('Não verificado')
+print('O resultado da divisão acima é {} e o resto é {}'.format(soma2, resto2))
