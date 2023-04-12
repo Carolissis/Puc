80c6785594f2b65d3f2ca96d8e0f890ff8f57d9a
Carolissis <127221267+Carolissis@users.noreply.github.com>
2023-04-12 10:05:33 -0300

    published changes

---
+++ exercicios curso3.py/ex075.py
@@ -0,0 +1,12 @@
+num = ()
+
+for c in range(1,4):
+    x = int(input(f'Digite o {}º valor: '))
+    num.append(x)
+if 9 in num:
+    print(f'Tem {num.count(9)} números 9')
+else:
+    print('O número 9 não foi digitado')
+
+if 3 in num:
+    print(f'O nú')
