867ff0495bb9ac9a175ee983ee56fb24d97dac6b
Carolissis <caroolassissss@gmail.com>
2023-04-13 15:47:44 -0300

    ex 75 e 76

--- exercicios curso3.py/ex075.py
+++ exercicios curso3.py/ex075.py
@@ -1,12 +1,15 @@
-num = ()
-
-for c in range(1,4):
-    x = int(input(f'Digite o {}º valor: '))
-    num.append(x)
+num = (int(input('Digite o 1º número: ')),
+       int(input('Digite o 2º número: ')),
+       int(input('Digite o 3º número: ')),
+       int(input('Digite o 4º número: ')))
 if 9 in num:
     print(f'Tem {num.count(9)} números 9')
 else:
     print('O número 9 não foi digitado')
 
 if 3 in num:
-    print(f'O nú')
+    print(f'O número 3 aparece na posição {num.index(3)}')
+
+for n in num:
+    if n % 2 == 0:
+        print(f'O numero {n} é par')
