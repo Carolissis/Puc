6b9d07a65add6fa5f73cd90edcaa994e9adb66d7
Carolissis <caroolassissss@gmail.com>
2023-04-26 10:58:08 -0300

    ex 06

---
+++ PUC/Aula RA/lista3_listas.py/ex06.py
@@ -0,0 +1,15 @@
+lista = []
+soma = 0
+
+while True:
+    num = int(input('Digite um valor: '))
+    lista.append(num)
+    resp = str(input('Deseja continuar? [S/N]\nR:')).lower().strip()
+    if resp in 'n':
+        break 
+
+for n in lista:
+    soma += n
+
+print(f'Sua lista é {lista}')
+print(f'A média dessee valores é: {soma / len(lista):.2f}')
