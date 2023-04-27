8b70f44a1529aadb32efe083dabe0191a9aec215
Carolissis <caroolassissss@gmail.com>
2023-04-27 07:44:59 -0300

    exercício 91

--- curso.em.video.python/mundo3.py/ex091.py
+++ curso.em.video.python/mundo3.py/ex091.py
@@ -2,10 +2,10 @@ from random import randint
 from time import sleep 
 from operator import itemgetter
 
-jogo = {'jogador1' : randint(1, 6),
-        'jogador2' : randint(1, 6),
-        'jogador3' : randint(1, 6),
-        'jogador4' : randint(1, 6)} 
+jogo = {'1º jogador' : randint(1, 6),
+        '2º jogador' : randint(1, 6),
+        '3º jogador' : randint(1, 6),
+        '4º jogador4' : randint(1, 6)} 
 ranking = list()
 print('Valores sorteados')
 for k, v in jogo.items():
@@ -17,3 +17,5 @@ print('='*20)
 print('RANKING JOGADORES')
 for i, v in enumerate(ranking):
     print(f'{i+1}º lugar: {v[0]} com {v[1]}')
+
+    #teste
