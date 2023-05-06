ea7350f8c50f922597e2492912542e3cd5cba1a3
Carolissis <caroolassissss@gmail.com>
2023-05-06 10:44:30 -0300

    112

--- curso.em.video.python/modularizacao/ex 112/teste/uteis/condicao.py
+++ curso.em.video.python/modularizacao/ex 112/teste/uteis/condicao.py
@@ -2,7 +2,7 @@ def leiadinheiro(msg):
     valido = False
     while not valido:
         entrada = str(input(msg)).replace(',', '.').strip()
-        if entrada.isalpha() or entrada == '':
+        if entrada.isalpha() or entrada == '' or entrada.isalnum():
             print(f'\033[31mERRO: \"{entrada}\"é um preço inválido!\033[m')
         else:
             valido = True
