35837d46b0b1cb03bf4cf4b4e731ab86d9a872ad
Carolissis <caroolassissss@gmail.com>
2023-05-06 09:44:21 -0300

    ex 110

---
+++ curso.em.video.python/modularizacao/ex 110/moeda110.py
@@ -0,0 +1,8 @@
+# Organizando formatcao 108
+
+import umoeda
+
+num = float(input('Digite um valor:\nR$'))
+aum = int(input('Taxa de aumento: '))
+des = int(input('Taxa de desconto: '))
+umoeda.resumo(num, aum, des)
