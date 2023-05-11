3fa5c7ad9f4f2e1ec1c32ba1d2fc7d82c3978bf1
Carolissis <caroolassissss@gmail.com>
2023-05-11 09:41:06 -0300

    ex 11

--- PUC/Aula RA/lista5_funcoes/ex11.py
+++ PUC/Aula RA/lista5_funcoes/ex11.py
@@ -1,9 +1,14 @@
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
+def data_extensa(a, b, c):
+    mese = meses[b-1]
+    return f'{a} de {mese} de {c}'
 
 data = []
 entrada = input('Digite a data (DD/MM/AAAA): ')
 data.extend(entrada.split('/'))
-dia = data[0]
-mes = data[1]
-ano = data[2]
-print(dia,mes,ano)
+dia = int(data[0])
+mes = int(data[1])
+ano = int(data[2])
+
+print(data_extensa(dia,mes,ano))
