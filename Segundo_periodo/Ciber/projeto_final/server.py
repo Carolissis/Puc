a9eca4001609477136c17dad46ff15eb4642b7ef
Caroline Assis <caroolassissss@gmail.com>
2023-11-14 10:47:37 -0300

    codigo pronto

--- Segundo_periodo/Ciber/projeto_final/server.py
+++ Segundo_periodo/Ciber/projeto_final/server.py
@@ -22,39 +22,65 @@ def broadcast(message):
     for cliente in clientes:
         cliente.send(message)
 
-def handle(cliente):
-    index = clientes.index(cliente)
-    nome = nomes[index]
+def send_message(sender_socket, message):
+    nome_remetente, _, content = message.partition(': ')
 
+    if content.startswith("/privado"):
+        try:
+            _, nome_destinatario, mens_privada = content.split(maxsplit=2)
+            if nome_destinatario in nomes:
+                index = nomes.index(nome_destinatario)
+                destinatario_socket = clientes[index]
+                destinatario_socket.send(f'Mensagem privada de {nome_remetente}: {mens_privada}'.encode('ascii'))
+            else:
+                sender_socket.send(f'Usuário {nome_destinatario} não encontrado.'.encode('ascii'))
+        except ValueError:
+            sender_socket.send('Formato de mensagem privada inválido. Use: /private [nome] [mensagem]'.encode('ascii'))
+    else:
+        broadcast_message = f'{nome_remetente}: {content}'
+        for client_socket in clientes:
+            if client_socket != sender_socket:
+                client_socket.send(broadcast_message.encode('ascii'))
+
+
+def handle(cliente):
     while True:
         try:
-            message = cliente.recv(1024)
-            broadcast(message)
+            message = cliente.recv(1024).decode('ascii')
+            if message:
+                send_message(cliente, message)
+            else:
+                handle_disconnection(cliente)
+                break
         except:
-            clientes.remove(cliente)
-            nomes.remove(nome)
-            cliente.close()
-            broadcast(f'{nome} saiu do chat!'.encode('ascii'))
-            print(f'{nome} se desconectou')
+            handle_disconnection(cliente)
             break
+
+def handle_disconnection(cliente):
+    index = clientes.index(cliente)
+    nome = nomes[index]
+    broadcast(f'{nome} saiu do chat!'.encode('ascii'))
+    print(f'{nome} se desconectou')
+    clientes.remove(cliente)
+    cliente.close()
+    nomes.remove(nome)
+
 
 def receive():
     while True:
-        cliente, addr = SERVER.accept()
+        cliente, addr = SERVER.accept()  
 
         cliente.send('NICK'.encode('ascii'))
         nome = cliente.recv(1024).decode('ascii')
         nomes.append(nome)
         clientes.append(cliente)
 
-        print(f'Conexão estabelecida com {nome}')  
+        print(f'Conexão estabelecida com {nome} de {addr[0]}: {addr[1]}') 
 
-        print(f'Nome do cliente é {nome}!')
         broadcast(f'{nome} entrou no chat!'.encode('ascii'))
         cliente.send('Conectado ao servidor!'.encode('ascii'))
 
-        thread = threading.Thread(target=handle, args=(cliente))
+        thread = threading.Thread(target=handle, args=(cliente,))
         thread.start()
-
 
 receive()
