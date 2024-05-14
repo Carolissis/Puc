7b2ddba0f5608d01608817a731143894eb18d991
Caroline Assis <caroolassissss@gmail.com>
2024-05-14 20:51:54 -0300

    ajuste trab ra2 ciber map direto

--- Terceiro_periodo/Ciber/map_direto.py
+++ Terceiro_periodo/Ciber/map_direto.py
@@ -5,8 +5,36 @@ def inicializar_cache(tam_cache):
     return cache
 
 def imprimir_cache(cache):
-    print(f"{'Posição da Memória':<20} | {'Valor':<10}")
-    print("-" * 35)
+    print(f"{'Pos Cache':<10} | {'Pos Memória':<10}")
+    print("-" * 25)
     for posicao, valor in cache.items():
-        print(f"{posicao:<20} | {valor:<10}")
+        print(f"{posicao:>10} | {valor:>10}")
 
+def mapeamento_direto(tam_cache, pos_memoria):
+    cache = inicializar_cache(tam_cache)
+    print("Cache Inicial")
+    print(f"{'Tam Cache: ':<10} {tam_cache:<10}")
+    imprimir_cache(cache)
+
+    hits = 0
+    misses = 0
+
+    for i in range(len(pos_memoria)):
+        pos_cache = pos_memoria[i] % tam_cache
+
+        print(f"Linha {pos_cache} | Posição da memória {pos_memoria[i]}")
+
+        if cache[pos_cache] == pos_memoria[i]:
+            hits += 1
+            print("Status: Hit")
+        else:
+            misses +=1
+            print("Status: Miss")
+            cache[pos_cache] = pos_memoria[i]
+        
+        imprimir_cache(cache)
+
+    print(f"Total de Misses: {misses}")
+    print(f"Total de Hits: {hits}")
+
+mapeamento_direto(5, [33,3,11,5])     
