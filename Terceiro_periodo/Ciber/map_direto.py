41e55f41db6f454e8f2498ff50c7bcad3d14375a
Caroline Assis <caroolassissss@gmail.com>
2024-05-15 18:15:37 -0300

    ex map direto e ajustes nos prints

--- Terceiro_periodo/Ciber/map_direto.py
+++ Terceiro_periodo/Ciber/map_direto.py
@@ -5,6 +5,7 @@ def inicializar_cache(tam_cache):
     return cache
 
 def imprimir_cache(cache):
+    print(f"{'Tamanho da Cache: ':<10} {len(cache):<10}")
     print(f"{'Pos Cache':<10} | {'Pos Memória':<10}")
     print("-" * 25)
     for posicao, valor in cache.items():
@@ -13,7 +14,6 @@ def imprimir_cache(cache):
 def mapeamento_direto(tam_cache, pos_memoria):
     cache = inicializar_cache(tam_cache)
     print("Cache Inicial")
-    print(f"{'Tam Cache: ':<10} {tam_cache:<10}")
     imprimir_cache(cache)
 
     hits = 0
@@ -22,7 +22,7 @@ def mapeamento_direto(tam_cache, pos_memoria):
     for i in range(len(pos_memoria)):
         pos_cache = pos_memoria[i] % tam_cache
 
-        print(f"Linha {pos_cache} | Posição da memória {pos_memoria[i]}")
+        print(f"Linha {i} | Posição da memória desejada {pos_memoria[i]}")
 
         if cache[pos_cache] == pos_memoria[i]:
             hits += 1
@@ -34,7 +34,16 @@ def mapeamento_direto(tam_cache, pos_memoria):
         
         imprimir_cache(cache)
 
+    totalAcesso = misses + hits
+    taxaAcerto = (hits / totalAcesso) * 100
+    print(f"Conectividade em Sistemas Ciberfísicos - Mapeamento Direto")
+    print(f"Total de acessos: {totalAcesso}")
     print(f"Total de Misses: {misses}")
     print(f"Total de Hits: {hits}")
+    print(f"Taxa de acertos (hits): {taxaAcerto :.2f}%")
 
-mapeamento_direto(5, [33,3,11,5])     
+# mapeamento_direto(5, [33,3,11,5])     
+# mapeamento_direto(5, [0,1,2,3,4,5,6])     
+# mapeamento_direto(5, [0,1,2,2,22,32,42,20,1,10,11,12,13])     
+# mapeamento_direto(5, [1,6,1,11,1,16,1,21,1,26])     
+mapeamento_direto(4, [3,7,11,15,3,19,11])   
